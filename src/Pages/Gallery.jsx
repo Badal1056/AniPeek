6b6cc773177b328a32a7700488976ef5355f7807
Badal183 <badaljha183@gmai.com>
2025-04-30 18:21:18 +0530

    All issues fixed

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -19,35 +19,35 @@ function Gallery() {
     }, [id])
 
     return (
-        <div>
-            <div className="back">
-                <Link to="/">
-                    <i className="fas fa-arrow-left"></i>
-                    Back to Home
-                </Link>
+            <div classname="gallery">
+                <div className="back">
+                    <Link to="/">
+                        <i className="fas fa-arrow-left"></i>
+                        Back to Home
+                    </Link>
+                </div>
+                <div className="big-image">
+                    <img src={pictures[index]?.jpg.image_url} alt="" />
+                </div>
+                <div className="small-images">
+                    {pictures?.map((picture, i) => {
+                        return <div className="image-con" onClick={()=>{
+                            handleImageClick(i)
+                        }} key={i}>
+                            <img 
+                                src={picture?.jpg.image_url}
+                                style={{
+                                    border: i === index ? "3px solid #27AE60" : "3px solid #e5e7eb",
+                                    filter: i === index ? 'grayscale(0)' : 'grayscale(60%)',
+                                    transform: i === index ? 'scale(1.1)' : 'scale(1)',
+                                    transition: 'all .3s ease-in-out'
+                                }}
+                                alt="" 
+                            />
+                        </div>
+                    })}
+                </div>
             </div>
-            <div className="big-image">
-                <img src={pictures[index]?.jpg.image_url} alt="" />
-            </div>
-            <div className="small-images">
-                {pictures?.map((picture, i) => {
-                    return <div className="image-con" onClick={()=>{
-                        handleImageClick(i)
-                    }} key={i}>
-                        <img 
-                            src={picture?.jpg.image_url}
-                            style={{
-                                border: i === index ? "3px solid #27AE60" : "3px solid #e5e7eb",
-                                filter: i === index ? 'grayscale(0)' : 'grayscale(60%)',
-                                transform: i === index ? 'scale(1.1)' : 'scale(1)',
-                                transition: 'all .3s ease-in-out'
-                            }}
-                            alt="" 
-                        />
-                    </div>
-                })}
-            </div>
-        </div>
     )
 }
 
